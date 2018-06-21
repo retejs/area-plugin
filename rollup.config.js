a6dcbf0d70913ce277a663ae99ce29ef99311540
Vitaliy Stoliarov <huccah@gmail.com>
2018-06-21 19:22:48 +0300

    initial

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+export default {
+    input: 'index.js',
+    output: {
+        file: 'build/index.js',
+        name: 'AreaPlugin',
+        format: 'umd',
+        sourcemap: true
+    },
+    plugins: []
+}
