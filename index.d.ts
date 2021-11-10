130172a305f046d7c9ee7e7f15b709001bd6f393
not-relevant-is-it <joel.edwardson1@gmail.com>
2021-11-10 12:01:21 +0000

    embedded install options type so typescript can infer types on installation

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,13 @@
 import { Plugin as RetePlugin } from 'rete/types/core/plugin';
 import { NodeEditor, Node } from 'rete';
 
-type InstallOptions = {
-  background?: boolean, // show background grid, default false
-  snap?: boolean, // snap nodes to grid, default false
-  scaleExtent?: true | {min: number, max: number} // restrict zoom min/max - setting to true will use defaults of min=0.1, max=1
-  translateExtent?: true | {width: number, height: number} // restrict translation via width and height - setting to true will use defaults of width=5000, height=4000
-}
 export interface AreaPlugin extends RetePlugin {
-  install: (editor: NodeEditor, options: InstallOptions) => void,
+  install: (editor: NodeEditor, options: {
+    background?: boolean, // show background grid, default false
+    snap?: boolean, // snap nodes to grid, default false
+    scaleExtent?: true | {min: number, max: number} // restrict zoom min/max - setting to true will use defaults of min=0.1, max=1
+    translateExtent?: true | {width: number, height: number} // restrict translation via width and height - setting to true will use defaults of width=5000, height=4000
+  }) => void,
   zoomAt: (editor: NodeEditor, nodes?: Node[]) => void // zoom to centre of nodes
 }
 declare const _default: AreaPlugin;
