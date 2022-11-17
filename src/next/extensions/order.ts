4db9a60ef95ffe34db32254fdcb64263dfb10c4a
Ni55aN <huccah@gmail.com>
2022-11-17 17:31:45 +0200

    order connection elements (replaces z-index -1)

--- src/next/extensions/order.ts
+++ src/next/extensions/order.ts
@@ -2,13 +2,22 @@ import { BaseSchemes } from 'rete'
 
 import { AreaPlugin } from '..'
 
-export function simpleNodesOrder(plugin: AreaPlugin<BaseSchemes, any>) {
+export function simpleNodesOrder<T>(plugin: AreaPlugin<BaseSchemes, T>) {
+    // eslint-disable-next-line max-statements
     plugin.addPipe(context => {
+        if (!('type' in context)) return context
         if (context.type === 'nodepicked') {
             const view = plugin.nodeViews.get(context.data.id)
 
             if (view) {
                 plugin.area.appendChild(view.element)
+            }
+        }
+        if (context.type === 'connectioncreated') {
+            const view = plugin.connectionViews.get(context.data.id)
+
+            if (view) {
+                plugin.area.element.prepend(view)
             }
         }
         return context
