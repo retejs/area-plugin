1534e75598722d028c7910b24498beb7a9234fcb
Ni55aN <huccah@gmail.com>
2023-02-20 20:28:06 +0200

    resize & translate methods

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ export type Area2D<Schemes extends BaseSchemes> =
     | { type: 'nodetranslate', data: { id: string } & NodeTranslateEventParams }
     | { type: 'nodetranslated', data: { id: string } & NodeTranslateEventParams }
     | { type: 'translate', data: { position: Position } }
-    | { type: 'contextmenu', data: { event: MouseEvent, context: 'root' | Schemes['Node'] } }
+    | { type: 'contextmenu', data: { event: MouseEvent, context: 'root' | Schemes['Node'] | Schemes['Connection'] } }
     | { type: 'pointerdown', data: { position: Position, event: PointerEvent }}
     | { type: 'pointermove', data: { position: Position, event: PointerEvent }}
     | { type: 'pointerup', data: { position: Position, event: PointerEvent }}
@@ -36,15 +36,16 @@ export type Area2D<Schemes extends BaseSchemes> =
     | { type: 'unmount', data: { element: HTMLElement } }
     | { type: 'nodedragged', data: Schemes['Node'] }
     | { type: 'resized', data: { event: Event } }
+    | { type: 'noderesize', data: { id: string } & NodeResizeEventParams }
     | { type: 'noderesized', data: { id: string } & NodeResizeEventParams }
 
 export type Area2DInherited<Schemes extends BaseSchemes, ExtraSignals = never> = [Area2D<Schemes> | ExtraSignals, Root<Schemes>]
 
 export class AreaPlugin<Schemes extends BaseSchemes, ExtraSignals = never> extends Scope<Area2D<Schemes> | ExtraSignals, [Root<Schemes>]> {
-  nodeViews = new Map<NodeId, NodeView>()
+  public nodeViews = new Map<NodeId, NodeView>()
   public connectionViews = new Map<ConnectionId, ConnectionView>()
-  area: Area
-  elements = new ElementsHolder<HTMLElement, RenderData<Schemes> & RenderMeta>()
+  public area: Area
+  private elements = new ElementsHolder<HTMLElement, RenderData<Schemes> & RenderMeta>()
 
   constructor(public container: HTMLElement) {
     super('area')
@@ -94,24 +95,23 @@ export class AreaPlugin<Schemes extends BaseSchemes, ExtraSignals = never> exten
   }
 
   private addNodeView(node: Schemes['Node']) {
-    const id = node.id
+    const { id } = node
     const view = new NodeView(
       () => this.area.transform.k,
       {
         picked: () => this.emit({ type: 'nodepicked', data: { id } }),
         translated: data => this.emit({ type: 'nodetranslated', data: { id, ...data } }),
-        dragged: () => this.emit({ type: 'nodedragged', data: node })
+        dragged: () => this.emit({ type: 'nodedragged', data: node }),
+        contextmenu: event => this.emit({ type: 'contextmenu', data: { event, context: node } }),
+        resized: ({ size }) => this.emit({ type: 'noderesized', data: { id: node.id, size } })
       },
       {
-        translate: data => this.emit({ type: 'nodetranslate', data: { id, ...data } })
+        translate: data => this.emit({ type: 'nodetranslate', data: { id, ...data } }),
+        resize: ({ size }) => this.emit({ type: 'noderesize', data: { id: node.id, size } })
       }
     )
 
-    view.element.addEventListener('contextmenu', event => {
-      this.emit({ type: 'contextmenu', data: { event, context: node } })
-    })
-
-    this.nodeViews.set(node.id, view)
+    this.nodeViews.set(id, view)
     this.area.appendChild(view.element)
 
     this.emit({
@@ -154,10 +154,22 @@ export class AreaPlugin<Schemes extends BaseSchemes, ExtraSignals = never> exten
     }
   }
 
-  public update(type: GetRenderTypes<Area2D<Schemes>> | GetRenderTypes<ExtraSignals>, id: string) {
+  public async update(type: GetRenderTypes<Area2D<Schemes>> | GetRenderTypes<ExtraSignals>, id: string) {
     const data = this.elements.get(type, id)
 
-    if (data) this.emit({ type: 'render', data })
+    if (data) await this.emit({ type: 'render', data })
+  }
+
+  public async resize(id: NodeId, width: number, height: number) {
+    const view = this.nodeViews.get(id)
+
+    if (view) return await view.resize(width, height)
+  }
+
+  public async translate(id: NodeId, { x, y }: Position) {
+    const view = this.nodeViews.get(id)
+
+    if (view) return await view.translate(x, y)
   }
 
   public destroy() {
--- src/node-view.ts
+++ src/node-view.ts
@@ -2,14 +2,17 @@ import { Drag } from './drag'
 import { Position, Size } from './types'
 
 export type NodeTranslateEventParams = { position: Position, previous: Position }
-export type NodeResizeEventParams = { size: Size, previous: Size }
+export type NodeResizeEventParams = { size: Size }
 
 type Events = {
   picked: () => void
   translated: (params: NodeTranslateEventParams) => Promise<unknown | boolean>
   dragged: () => void
+  contextmenu: (event: MouseEvent) => void
+  resized: (params: NodeResizeEventParams) => Promise<unknown | boolean>
 }
 type Guards = {
+  resize: (params: NodeResizeEventParams) => Promise<unknown | boolean>
   translate: (params: NodeTranslateEventParams) => Promise<unknown | boolean>
 }
 
@@ -23,6 +26,8 @@ export class NodeView {
     this.element.style.position = 'absolute'
     this.position = { x: 0, y: 0 }
     this.translate(0, 0)
+
+    this.element.addEventListener('contextmenu', event => this.events.contextmenu(event))
 
     this.dragHandler = new Drag(
       this.element,
@@ -39,18 +44,33 @@ export class NodeView {
   }
 
   public translate = async (x: number, y: number) => {
-        type Params = undefined | { data: NodeTranslateEventParams }
-        const previous = { ...this.position }
-        const translation = await this.guards.translate({ previous, position: { x, y } }) as Params
+    type Params = undefined | { data: NodeTranslateEventParams }
+    const previous = { ...this.position }
+    const translation = await this.guards.translate({ previous, position: { x, y } }) as Params
 
-        if (!translation) return false
+    if (!translation) return false
 
-        this.position = { ...translation.data.position }
-        this.element.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`
+    this.position = { ...translation.data.position }
+    this.element.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`
 
-        await this.events.translated({ position: this.position, previous })
+    await this.events.translated({ position: this.position, previous })
 
-        return true
+    return true
+  }
+
+  public resize = async (width: number, height: number) => {
+    const size = { width, height }
+
+    if (!(await this.guards.resize({ size }))) return false
+
+    const el = this.element.children.item(0)
+
+    if (!el || !(el instanceof HTMLElement)) return
+
+    el.style.width = `${width}px`
+    el.style.height = `${height}px`
+
+    await this.events.resized({ size })
   }
 
   public destroy() {
