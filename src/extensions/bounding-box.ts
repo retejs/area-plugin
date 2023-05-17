c6a1f28d69fb589213e6848cbe82d7fd7e534c00
Ni55aN <huccah@gmail.com>
2023-05-17 21:59:31 +0300

    extend BaseAreaPlugin by getBoundingBox

--- src/extensions/bounding-box.ts
+++ src/extensions/bounding-box.ts
@@ -1,11 +1,11 @@
 import { BaseSchemes, NodeEditor } from 'rete'
 
-import { AreaPlugin } from '..'
+import { BaseAreaPlugin } from '../base'
 import { getBoundingBox as getBBox } from '../utils'
 import { NodeRef } from './shared/types'
 import { getNodesRect } from './shared/utils'
 
-export function getBoundingBox<Schemes extends BaseSchemes, K>(plugin: AreaPlugin<Schemes, K>, nodes: NodeRef<Schemes>[]) {
+export function getBoundingBox<Schemes extends BaseSchemes, K>(plugin: BaseAreaPlugin<Schemes, K>, nodes: NodeRef<Schemes>[]) {
   const editor = plugin.parentScope<NodeEditor<Schemes>>(NodeEditor)
   const list = nodes.map(node => typeof node === 'object' ? node : editor.getNode(node))
   const rects = getNodesRect(list, plugin.nodeViews)
--- src/extensions/shared/utils.ts
+++ src/extensions/shared/utils.ts
@@ -1,9 +1,8 @@
-import { NodeId } from 'rete'
-
+import { BaseAreaPlugin } from '../../base'
 import { NodeView } from '../../node-view'
 import { SchemesWithSizes } from './types'
 
-export function getNodesRect(nodes: SchemesWithSizes['Node'][], views: Map<NodeId, NodeView>) {
+export function getNodesRect<S extends SchemesWithSizes, K>(nodes: S['Node'][], views: BaseAreaPlugin<S, K>['nodeViews']) {
   return nodes
     .map(node => ({ view: views.get(node.id) as NodeView, node }))
     .filter(item => item.view)
